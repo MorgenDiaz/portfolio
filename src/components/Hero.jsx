39be7af5bc50744a6b00c404330fb2b650e0b37c
Morgen Diaz <morgen@webslinger.works>
2022-12-17 13:05:08 -1000

    Set title of page to my name, created hero component. Installed and configured tailwind.

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="container">
+        <h1 className="font-semibold text-3xl mb-5">
+          Hi, my name is <span className="text-purple-900">Morgen Diaz</span>
+          <br />
+          Let me solve your coding troubles!
+        </h1>
+      </div>
+    </section>
+  );
+}
