142bc08a286eb2c935c1326371133f07acf7dfc3
Morgen Diaz <morgen@webslinger.works>
2022-12-17 15:05:42 -1000

    My name displays in a gradient color.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,18 @@ export default function Hero() {
     <section className="hero">
       <div className="container min-h-screen flex items-center border-b-0 bg-slate-50 font-normal text-gray-900 py-0 px-6 sm:px-20 mb-0 inset-0 sticky">
         <h1 className="font-bold leading-normal sm:leading-none text-4xl sm:text-5xl lg:text-6xl mb-12 text-center md:text-left ">
-          Hi, my name is <span className="text-purple-700">Morgen Diaz</span>
+          Hi, my name is{" "}
+          <span className="text-transparent bg-clip-text bg-gradient-to-br from-purple-800 to-purple-400">
+            Morgen Diaz
+          </span>
           <br />
           Let me solve your coding troubles!
         </h1>
+        <p className="flex content-center">
+          <a rel="noreferrer" href="#about" className="inline-block relative">
+            Know more
+          </a>
+        </p>
       </div>
     </section>
   );
