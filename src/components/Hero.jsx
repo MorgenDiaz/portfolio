af6f25545e3237f7c2c8453e57148af8825e506f
Morgen Diaz <morgen@webslinger.works>
2022-12-22 11:02:59 -1000

    Dialed in primary button styling. Improved heading font for portrait tablets. My name will not break apart when trying to refit heading for screen.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 const SECTION_DEFAULTS = "border-none px-4 py-20 phone-xs:px-0";
 const BUTTON_DEFAULTS =
-  "inline-block p-2 relative text-2xl tab-port:text-3xl font-semibold z-10 after:block after:absolute after:w-0 after:h-full after:left-0 after:bottom-0 after:-z-10 transition-all after:transition-all";
+  "inline-block py-0.5 px-4 relative text-2xl tab-port:text-3xl font-bold text-purple-800 z-10 after:block after:absolute after:w-0 after:h-full after:left-0 after:bottom-0 after:-z-10 transition-all after:transition-all";
 
 export default function Hero() {
   return (
@@ -9,19 +9,24 @@ export default function Hero() {
     >
       <div className="flex justify-center px-10 grow ">
         <div className="">
-          <h1 className="mb-12 text-4xl font-bold leading-normal text-center phone-xs:text-5xl phone:text-6xl tab-port:text-left">
+          <h1 className="mb-12 text-4xl font-bold leading-normal text-center phone-xs:text-5xl phone:text-5xl tab-port:text-left tab-land:text-6xl">
             Hi, my name is{" "}
-            <span className="text-transparent bg-clip-text bg-gradient-to-br from-purple-800 to-purple-400">
+            <span className="inline-block leading-normal text-transparent bg-clip-text bg-gradient-to-br from-purple-800 to-purple-400">
               Morgen Diaz
             </span>
             <br />
             Let me solve your coding troubles!
           </h1>
-          <p className="flex justify-center tab-port:justify-start ">
+          <p className="flex content-center justify-center tab-port:justify-start ">
             <a
               rel="noreferrer"
               href="#about"
-              className={`${BUTTON_DEFAULTS} border-2 border-purple-800 border-solid hover:border-transparent hover:text-white w-full after:bg-gradient-to-br from-purple-800 to-purple-400 hover:after:w-full`}
+              className={`
+                ${BUTTON_DEFAULTS}
+                border-2 border-purple-800 border-solid hover:border-transparent
+              hover:text-white
+                after:bg-gradient-to-br from-purple-800 to-purple-400 hover:after:w-full
+                `}
             >
               Know more
             </a>
